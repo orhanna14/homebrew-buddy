59c4e4730442404a82c53564ffb21b6918551801
Olivia McGoffin <olivia@thoughtbot.com>
2019-11-18 15:11:30 -0800

    Adding a 'Create a new User' feature
    Driven by a feature spec, new users can now click a button and sign up
    with their name and email in a form. Once the form is submitted, there
    is a success notice upon redirection to the homepage. The next step
    would be to add validation of uniqueness of name and email.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# Users Controller class
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    flash[:success] = 'You have successfully signed up!'
+    redirect_to '/'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
--- spec/features/create_a_user_spec.rb
+++ spec/features/create_a_user_spec.rb
@@ -1,8 +1,17 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.feature 'Create a User' do
-  scenario 'They click a button on the homepage that lets them sign up' do
-    visit  '/'
+RSpec.feature 'Create a User', type: :feature do
+  scenario 'New users can sign up' do
+    visit '/'
+    click_link 'Sign Up'
+    expect(page).to have_content 'Sign Up Here'
+
+    fill_in 'Name', with: 'MyFirstName'
+    fill_in 'Email', with: 'MyEmailAddress'
     click_button 'Sign Up'
+
+    expect(page).to have_text('You have successfully signed up!')
   end
 end
