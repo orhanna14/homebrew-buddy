58dd7e55397d5bebefa2aaec03dede8afbef4acc
Olivia McGoffin <olivia@thoughtbot.com>
2019-11-20 15:33:34 -0800

    Update feature spec to asset that buttons and links works as expected, and that the user is saved. Removed static text expectation

--- spec/features/create_a_user_spec.rb
+++ spec/features/create_a_user_spec.rb
@@ -7,11 +7,9 @@ RSpec.feature 'Create a User', type: :feature do
     user = User.new(name: 'Olivia', email: 'olivia@aivilo.com')
     visit '/'
     click_link 'Sign Up'
-    expect(page).to have_content 'Sign Up Here'
     fill_in 'Name', with: user.name
     fill_in 'Email', with: user.email
     click_button 'Sign Up'
-    expect(page).to have_text('You have successfully signed up!')
     assert user.save
   end
 end
