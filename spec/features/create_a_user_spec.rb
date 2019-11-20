cdc0d9c6f9b0af83e0a41217f96058e79522cc0f
Olivia McGoffin <olivia@thoughtbot.com>
2019-11-20 15:25:32 -0800

    Hound fixes

--- spec/features/create_a_user_spec.rb
+++ spec/features/create_a_user_spec.rb
@@ -5,15 +5,12 @@ require 'rails_helper'
 RSpec.feature 'Create a User', type: :feature do
   scenario 'New users can sign up with valid name and email' do
     user = User.new(name: 'Olivia', email: 'olivia@aivilo.com')
-
     visit '/'
     click_link 'Sign Up'
     expect(page).to have_content 'Sign Up Here'
-
     fill_in 'Name', with: user.name
     fill_in 'Email', with: user.email
     click_button 'Sign Up'
-
     expect(page).to have_text('You have successfully signed up!')
     assert user.save
   end
