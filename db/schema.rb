41db1067eb6746e0dc72b7fab67c247903c00a99
Olivia McGoffin <olivia@thoughtbot.com>
2019-11-21 17:00:39 -0800

    Modify tests to account for password addition Add ability to sign up and be signed in Configured devise to permit a name, email, and password from a new user.

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 2019_11_22_004444) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", default: "", null: false
@@ -24,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_11_22_004444) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
