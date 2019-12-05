8b724cf00a97f28177c511f959b8278fe721fa74
Olivia McGoffin <olivia@thoughtbot.com>
2019-12-05 11:40:14 -0800

    Rename factory

---
+++ spec/factories/user.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  sequence :name do |n|
+    "User#{n}"
+  end
+
+  sequence :email do |n|
+    "user#{n}@example.com"
+  end
+
+  factory :user do
+    name
+    email
+    password { 'password' }
+  end
+end
