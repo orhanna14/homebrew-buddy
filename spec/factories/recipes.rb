93257078ae989c7d7aead47a8d9df3830a8e5aff
Olivia McGoffin <olivia@thoughtbot.com>
2019-12-05 12:09:55 -0800

    Adding a recipe model

---
+++ spec/factories/recipes.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :recipe do
+    user
+    title { 'MyString' }
+    ingredients { 'MyText' }
+    instructions { 'MyText' }
+  end
+end
