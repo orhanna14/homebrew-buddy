d5f501cff72157277e242d13b2a10eabc6e6051b
Olivia McGoffin <olivia@thoughtbot.com>
2019-12-11 11:25:41 -0800

    Added rubocop with TB config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,13 +7,13 @@ Rails.application.configure do
 
   config.consider_all_requests_local = true
 
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+  if Rails.root.join("tmp", "caching-dev.txt").exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}",
     }
   else
     config.action_controller.perform_caching = false
@@ -24,7 +24,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
   config.action_mailer.perform_caching = false
 
   config.active_support.deprecation = :log
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
 
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
+    "Cache-Control" => "public, max-age=#{1.hour.to_i}",
   }
 
   config.consider_all_requests_local = true
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.action_controller.allow_forgery_protection = false
 
   config.active_storage.service = :test
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :test
