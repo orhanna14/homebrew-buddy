380b5cf6f4cb28f81ab41c840dcd26a58cc1a4db
Olivia McGoffin <olivia@thoughtbot.com>
2019-12-17 11:07:18 -0800

    Remove pictures model and controller

--- app/controllers/pictures_controller.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class PicturesController < ApplicationController
- def create
- end
-end
--- app/models/picture.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class Picture < ApplicationRecord
-  belongs_to :imageable, polymorphic: true
-end
