b660fa465230c9c76ac8ed61fb37be93faba8e06
Olivia McGoffin <olivia@thoughtbot.com>
2019-12-16 16:12:58 -0800

    Working spec! Changed how the file was uploaded

--- spec/system/signed_in_user_creates_a_recipe_spec.rb
+++ spec/system/signed_in_user_creates_a_recipe_spec.rb
@@ -18,4 +18,20 @@ RSpec.feature "A signed in user creates a recipe" do
 
     expect(page).to have_current_path("/")
   end
+
+  scenario 'and includes a picture' do
+    user = create(:user)
+    recipe = FactoryBot.build(:recipe)
+
+    sign_in_with user.email, user.password
+    visit '/'
+    click_link I18n.t('layouts.application.new_recipe')
+    fill_in 'Title', with: recipe.title
+    fill_in 'Ingredients', with: recipe.ingredients
+    fill_in 'Instruction', with: recipe.instructions
+    recipe.pictures.attach(io: File.open('public/apple-touch-icon.png'), filename: 'apple-touch-icon.png')
+    click_button 'Create Recipe'
+
+    expect(recipe.pictures.present?).to eq(true)
+  end
 end
