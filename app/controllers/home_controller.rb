f43eed8928ec92b1c9730b698750263236d34744
Olivia McGoffin <olivia@thoughtbot.com>
2019-12-23 16:01:23 -0800

    Moved request_api method into service object. This request can probably be improved

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   def index; end
 
   def search
-    breweries = find_brewery(params[:brewery])
+    breweries = BreweryClient.new(params[:brewery]).find_brewery(params[:brewery])
 
     unless breweries
      flash[:alert] = "Brewery not found"
@@ -13,28 +13,7 @@ class HomeController < ApplicationController
     end
 
     @brewery = breweries.first
+
     # TODO: Probably make this list all the breweries
-  end
-
-  private
-
-  def request_api(url)
-    response = Excon.get(
-      url,
-      headers: {
-        "X-RapidAPI-Host" => Rails.application.credentials.x_rapid_api[:host],
-        "X-RapidAPI-Key" => Rails.application.credentials.x_rapid_api[:key] ,
-      }
-   )
-
-    return nil if response.status != 200
-
-    JSON.parse(response.body)
-  end
-
-  def find_brewery(name)
-    request_api(
-"https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries/search?query=#{name}"
-    )
   end
 end
