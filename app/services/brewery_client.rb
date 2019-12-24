ee0aad1eb36cde4c942698cf1dc44284b8619f0a
Olivia McGoffin <olivia@thoughtbot.com>
2019-12-23 16:01:23 -0800

    Moved request_api method into service object. This request can probably be improved

---
+++ app/services/brewery_client.rb
@@ -0,0 +1,24 @@
+class BreweryClient
+  def initialize(name)
+    @name = name
+  end
+
+  def request_api(url)
+  response = Excon.get(
+    url,
+    headers: {
+      "X-RapidAPI-Host" => Rails.application.credentials.x_rapid_api[:host],
+      "X-RapidAPI-Key" => Rails.application.credentials.x_rapid_api[:key] ,
+    }
+  )
+
+  return nil if response.status != 200
+
+  JSON.parse(response.body)
+  end
+
+  def find_brewery(name)
+    request_api(
+      "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries/search?query=#{name}")
+  end
+end
