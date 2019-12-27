865d7c5dc425c6b9996193c0561751e6a3d04d27
Olivia McGoffin <olivia@thoughtbot.com>
2019-12-26 16:51:31 -0800

    Refactor system spec

--- spec/system/potential_user_browses_breweries_spec.rb
+++ spec/system/potential_user_browses_breweries_spec.rb
@@ -5,18 +5,24 @@ require "spec_helper"
 
 RSpec.feature "A potential user sends an external request to search breweries" do
   scenario "with valid parameters in the OpenBrewery DB Search feature" do
-    visit "/"
-    fill_in "brewery", with: "Anchor"
-    click_on "Search"
-
-    name = find("name")
-    expect(name).to have_content("Anchor Brewing Co")
+    brewery_name = "Anchor"
     uri = ("https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries/search")
+
+    visit "/"
+    fill_in "brewery", with: brewery_name
+    stub_request(:get, "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries/search").
+      with(
+        headers: {
+          "Accept" => "*/*",
+          "Host" => "brianiswu-open-brewery-db-v1.p.rapidapi.com:443",
+          "User-Agent" => "excon/0.71.1",
+        }).
+        to_return(status: 200, body: "{ 'name': 'Anchor Steam Brewery'}")
 
     connection = Excon.new(uri)
     get_response = connection.get
+    json = JSON.parse(get_response.data.to_json)
 
-    expect(get_response.data).to be_an_instance_of(Hash)
-
+    expect(json["body"]).to include(brewery_name)
   end
 end
