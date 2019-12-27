c2a8854846178a3aaac952f53fd5c457505a1c89
Olivia McGoffin <olivia@thoughtbot.com>
2019-12-27 14:22:32 -0800

    Update System spec using Service mock

--- spec/system/potential_user_browses_breweries_spec.rb
+++ spec/system/potential_user_browses_breweries_spec.rb
@@ -6,25 +6,25 @@ require "spec_helper"
 RSpec.feature "A potential user sends an external request to search breweries" do
   scenario "with valid parameters in the OpenBrewery DB Search feature" do
     brewery_name = "Anchor"
-    uri = "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries/search"
+    search_service = double("BreweryClient")
+    allow(search_service).to receive(:find_brewery).
+      and_return(JSON.parse(fake_json_response))
+    allow(BreweryClient).to receive(:new).and_return(search_service)
 
     visit "/"
     fill_in "brewery", with: brewery_name
-    stub_request(:get,
-                 "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries/search").
-      with(
-        headers: {
-          "Accept" => "*/*",
-          "Host" => "brianiswu-open-brewery-db-v1.p.rapidapi.com:443",
-          "User-Agent" => "excon/0.71.1",
-        },
-      ).
-      to_return(status: 200, body: "{ 'name': 'Anchor Steam Brewery'}")
+    click_on "Search"
 
-    connection = Excon.new(uri)
-    get_response = connection.get
-    json = JSON.parse(get_response.data.to_json)
+    expect(page).to have_content(brewery_name)
+  end
 
-    expect(json["body"]).to include(brewery_name)
+  def fake_json_response
+    "[{\"id\":314,\"name\":\"Anchor Brewing Co\",\"brewery_type\":\"large\",
+    \"street\":\"1705 Mariposa St\",\"city\":\"San Francisco\",
+    \"state\":\"California\",\"postal_code\":\"94107-2334\",
+    \"country\":\"United States\",\"longitude\":\"-122.4011065\",
+    \"latitude\":\"37.7630772\",\"phone\":\"4158638350\",
+    \"website_url\":\"http://www.anchorbrewing.com\",
+    \"updated_at\":\"2018-08-23T23:24:25.576Z\",\"tag_list\":[]}]"
   end
 end
