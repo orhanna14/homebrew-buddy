3c6f964bd2492f9ab3c9c292975ad7606e56e509
Olivia McGoffin <olivia@thoughtbot.com>
2019-12-27 15:26:58 -0800

    Added a  directory, moved the stubbed request in there from  to clearly demonstrate where I am looking for this request to be had. The test is passing ATM, but I'd like to make sure this is what I am looking for/is this truly testing what I would like?

---
+++ spec/services/brewery_client_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe BreweryClient do
+  describe ".find_brewery" do
+    it "returns the first brewery with the name sent in the search parameter" do
+      brewery_name = "Anchor"
+
+      stub_request(:any, "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries/search?query=#{brewery_name}").
+        with(
+          headers: {
+            "Host"=>"brianiswu-open-brewery-db-v1.p.rapidapi.com:443",
+            "X-Rapidapi-Host"=>"brianiswu-open-brewery-db-v1.p.rapidapi.com",
+            "X-Rapidapi-Key"=>"d06411ee49mshcbcdc80cdd60989p1f0313jsn80c1ad5cec44",
+          }).to_return(status: 200, body: "name: Anchor Steam Brewery", headers: {})
+
+      result = described_class.find_brewery(brewery_name)
+
+      expect(result).to include(brewery_name)
+      # TODO: Is this truly doing what I want/expect it to do?
+    end
+  end
+end
