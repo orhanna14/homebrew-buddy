3364083fd863f6e48634ea687919468d2ca52cbb
Olivia McGoffin <olivia@thoughtbot.com>
2019-12-27 14:46:32 -0800

    Move alert to I18n

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,13 +7,11 @@ class HomeController < ApplicationController
     breweries =
       BrewerySearch.new(params[:brewery]).find_brewery(params[:brewery])
     unless breweries
-      flash[:alert] = "Brewery not found"
-      # TODO: Move this alert to I18n.
+      flash[:alert] = t(".not_found")
       return render action: :index
     end
 
     @brewery = breweries.first
 
-    # TODO: Probably make this list all the breweries
   end
 end
