b263409020dcf38ee0f15865987ccae3dcb8893b
Olivia McGoffin <olivia@thoughtbot.com>
2019-12-31 14:53:23 -0800

    Add content type validations for user and recipe pictures

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class Recipe < ApplicationRecord
+  belongs_to :user
+  has_many_attached :pictures
+
   validates_presence_of :title
   validates_presence_of :ingredients
   validates_presence_of :instructions
-
-  belongs_to :user
-  has_many_attached :pictures
+  validates :pictures, allow_nil: true,
+                       content_type: %i[png jpg jpeg]
 
 end
