b05f5c05d8b2f998a52b58de69330b56852bbd83
Olivia McGoffin <olivia@thoughtbot.com>
2020-01-06 13:49:13 -0800

    Added pagination for browsing recipes

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem "rails", "~> 6.0.1"
 gem "sass-rails", ">= 6"
 gem "turbolinks", "~> 5"
 gem "webpacker", "~> 4.0"
+gem "will_paginate"
 
 group :development, :test do
   gem "byebug", platforms: %i[mri mingw x64_mingw]
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
   def index
     @user = current_user
     @recipes = Recipe.all.order("created_at DESC")
+    @recipes = Recipe.paginate(page: params[:page], per_page: 10)
   end
 
   def create
